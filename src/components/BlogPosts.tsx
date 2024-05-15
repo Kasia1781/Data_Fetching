ac202237080496de796f880a02f7f7d7e4e4d56e
Kasia <kasia1781@gmail.com>
2024-05-15 20:01:47 +0200

    fix: add ul tag in BlogPosts component

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -12,12 +12,14 @@ export default function BlogPosts({ posts }: BlogPostsProps) {
 	return (
 		<div id='blog-posts'>
 			<h1>Blog Posts</h1>
-			{posts.map((post) => (
-				<li key={post.id}>
-					<h2>{post.title}</h2>
-					<p>{post.text}</p>
-				</li>
-			))}
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>
+						<h2>{post.title}</h2>
+						<p>{post.text}</p>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
