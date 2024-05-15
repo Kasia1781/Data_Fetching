c5f27d77b01b3a48b757c32b0ddb81713dfcee84
Kasia <kasia1781@gmail.com>
2024-05-15 20:04:24 +0200

    use useState hook in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import { get } from './util/http';
+import { BlogPost } from './components/BlogPosts';
 
 function App() {
+	const [fetchedPosts, setFetchedPost] = useState<BlogPost[]>();
+
 	get('https://jsonplaceholder.typicode.com/posts');
 
 	return <h1>Data fetching!</h1>;
